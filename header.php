12d3aaab9ac66587ba379b56364277ee1384b6e4
Eric Mann <eric@eamann.com>
2011-07-28 07:24:37 -0700

    Clean up header

--- header.php
+++ header.php
@@ -9,37 +9,38 @@
 </head>
 <body <?php body_class(); ?>>
 
-<div id="header">
-	<div id="masthead">
-			<div id="branding">
-				<table width="820px" border="0" cellspacing="0" cellpadding="0">
-				<tbody>
-					<tr>
-						<td colspan="4">
-							<div id="p7TBM">
-								<div id="p7TBMroot">
-								<div id="p7TBMrootbox">
-									<ul class="p7TBMtext" style="padding-left:10px; background=http://watermarkcommunities.com/includes/TopNavs/p7tbm/img/bkgnd_master.gif; display:inline-block; line-height:10pt">
-										<li><a href="http://www.watermarkcommunities.com" id="p7TBMt01">Watermark Home</a></li>
-										<li><a href="http://www.watermarkcommunities.com/communities/" id="p7TBMt02">Find Your Watermark Community</a></li>
-										<li><a href="http://www.watermarkcommunities.com/wrc/" id="p7TBMt03">About Watermark</a></li>
-										<br />
-									</ul>
-								</div>
-							</div>
-							<!--[if lte IE 6]>
-							<style>#p7TBM div, #p7TBM ul, #p7TBM a {height:1%;}</style>
-							<![endif]-->
-						</div>
-					</td>
-				</tr>
-  				<tr></tr>
-			</tbody>
-			</table>
-		</div><!-- #branding -->
-	</div>
 
+<div id="header">
 	<div class="sleeve">
+		<div id="masthead">
+				<div id="branding">
+					<table width="820px" border="0" cellspacing="0" cellpadding="0">
+					<tbody>
+						<tr>
+							<td colspan="4">
+								<div id="p7TBM">
+									<div id="p7TBMroot">
+									<div id="p7TBMrootbox">
+										<ul class="p7TBMtext" style="padding-left:10px; background=http://watermarkcommunities.com/includes/TopNavs/p7tbm/img/bkgnd_master.gif; display:inline-block; line-height:10pt">
+											<li><a href="http://www.watermarkcommunities.com" id="p7TBMt01">Watermark Home</a></li>
+											<li><a href="http://www.watermarkcommunities.com/communities/" id="p7TBMt02">Find Your Watermark Community</a></li>
+											<li><a href="http://www.watermarkcommunities.com/wrc/" id="p7TBMt03">About Watermark</a></li>
+											<br />
+										</ul>
+									</div>
+								</div>
+								<!--[if lte IE 6]>
+								<style>#p7TBM div, #p7TBM ul, #p7TBM a {height:1%;}</style>
+								<![endif]-->
+							</div>
+						</td>
+					</tr>
+	  				<tr></tr>
+				</tbody>
+				</table>
+			</div><!-- #branding -->
+		</div>
+
 		<span class="thrive-logo"></span>
 		<h1><a href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 		<?php if ( get_bloginfo( 'description' ) ) : ?>
